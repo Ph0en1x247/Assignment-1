c92a483868205b3ae2ca5c7e62f5a2482d8db8aa
Ph0en1x247 <matt.jacyk@gmail.com>
2018-01-31 13:50:20 -0700

    Assignment 1: algorithm analysis (sorting)
    Sorting algorithm analysis

---
+++ Assign1/src/Assign1.java
@@ -0,0 +1,24 @@
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		// order, size, algorithm, outfile
+		for (String s: args) {
+			
+		}
+
+	}
+	private void Selection() {
+		
+	}
+	private void Insertion() {
+		
+	}
+	private void Merge() {
+		
+	}
+	private void Quick() {
+		
+	}
+
+}
